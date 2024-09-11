e0e3291b73403d1c6ceb5c7602e0027cd709808d
nguyenvukhang <brew4k@gmail.com>
2024-09-11 23:04:21 +0800

    Read with record batch

--- migrate.py
+++ migrate.py
@@ -1,25 +1,32 @@
 from pandas import DataFrame
-import pyarrow as pa
+import pyarrow as pa, os
 import pyarrow.parquet as pq
 
 
 def read_pq(p: str) -> DataFrame:
     return pq.read_table(p).to_pandas()
 
+
 def write_pq(df: DataFrame, p: str):
     pq.write_table(pa.Table.from_pandas(df), p)
 
 
-df = read_pq("~/.local/data/msr-cambridge/hm_1.parquet")
-df = read_pq("out.parquet")
-print(df.dtypes)
-df["Timestamp"] = df["Timestamp"].astype("UInt64")
-df["DiskNumber"] = df["DiskNumber"].astype("UInt32")
-df["Offset"] = df["Offset"].astype("UInt64")
-df["Size"] = df["Size"].astype("UInt64")
-df["ResponseTime"] = df["ResponseTime"].astype("UInt64")
-print(df.dtypes)
-print(df.max())
-print(2**31)
+def migrate(src: str):
+    df = read_pq(src)
+    print("BEFORE", df.dtypes)
+    df["Timestamp"] = df["Timestamp"].astype("UInt64")
+    df["DiskNumber"] = df["DiskNumber"].astype("UInt32")
+    df["Offset"] = df["Offset"].astype("UInt64")
+    df["Size"] = df["Size"].astype("UInt32")
+    df["ResponseTime"] = df["ResponseTime"].astype("UInt64")
+    print("AFTER", df.dtypes)
 
-write_pq(df, "out.parquet")
+    write_pq(df, src.replace(".parquet", ".typed.parquet"))
+
+
+data_dir = "/Users/khang/.local/data/msr-cambridge"
+for x in os.listdir(data_dir):
+    if not (x.endswith("parquet") and "typed" not in x):
+        continue
+    src = os.path.join(data_dir, x)
+    migrate(src)
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,70 @@
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
-use parquet::file::reader::{FileReader, SerializedFileReader};
-use parquet::record::{reader::RowIter, Row, RowAccessor};
 
-use arrow::array::{AsArray, GenericListArray};
-use arrow_array::UInt64Array;
+use arrow::array::{Array, StringArray, UInt32Array, UInt64Array};
+use arrow::record_batch::RecordBatch;
 
 use std::fs::File;
 use std::path::{Path, PathBuf};
+use std::time::Instant;
+
+#[derive(Debug)]
+enum TraceKind {
+    Read,
+    Write,
+}
+
+#[derive(Debug)]
+struct Trace {
+    timestamp: u64,
+    kind: TraceKind,
+    addr: u64,
+    size: u32,
+}
+
+fn parse_record_batch(mut rb: RecordBatch) -> Vec<Trace> {
+    let x = rb.remove_column(5);
+    let sizes = x.as_any().downcast_ref::<UInt32Array>().unwrap();
+    let x = rb.remove_column(4);
+    let addrs = x.as_any().downcast_ref::<UInt64Array>().unwrap();
+    let x = rb.remove_column(3);
+    let kinds = x.as_any().downcast_ref::<StringArray>().unwrap();
+    let x = rb.remove_column(0);
+    let timestamps = x.as_any().downcast_ref::<UInt64Array>().unwrap();
+    let iter = timestamps.iter().zip(kinds).zip(addrs).zip(sizes);
+    let mut vec = Vec::with_capacity(timestamps.len());
+    for (((t, kind), addr), size) in iter {
+        let t = t.unwrap();
+        let addr = addr.unwrap();
+        let size = size.unwrap();
+        let kind = match kind.unwrap().as_bytes()[0] {
+            b'R' => TraceKind::Read,
+            b'W' => TraceKind::Write,
+            v => panic!("Invalid trace kind: {v}"),
+        };
+        vec.push(Trace { timestamp: t, kind, addr, size });
+    }
+    vec
+}
 
 fn main() {
     let data_dir = Path::new("/Users/khang/.local/data/msr-cambridge");
-    let f = File::open(data_dir.join("hm_1.typed.parquet")).unwrap();
+    let f = File::open(data_dir.join("proj_2.typed.parquet")).unwrap();
     let builder = ParquetRecordBatchReaderBuilder::try_new(f).unwrap();
-    let mut reader = builder.build().unwrap();
+    let reader = builder.build().unwrap();
 
-    let mut record_batch = reader.next().unwrap().unwrap();
-    println!("record_batch: {:?}", record_batch.schema());
-    let x = record_batch.remove_column(4);
-    let offset_col = x.as_any().downcast_ref::<UInt64Array>().unwrap();
-    for x in offset_col {
-        println!("{x:?}");
+    let start = Instant::now();
+    for x in reader {
+        let rb = x.unwrap();
+        let traces = parse_record_batch(rb);
     }
-    for col in record_batch.columns() {
-        // println!("{:?}", col);
-    }
-    // println!("{:?}", record_batch);
+    println!("{:?}", Instant::elapsed(&start));
+
+    // let mut record_batch = reader.next().unwrap().unwrap();
+    // println!("record_batch: {:?}", record_batch.schema());
+    // let x = record_batch.remove_column(4);
+    // let addrs = x.as_any().downcast_ref::<UInt64Array>().unwrap();
+    // for col in record_batch.columns() {
+    //     // println!("{:?}", col);
+    // }
+    // // println!("{:?}", record_batch);
 }
