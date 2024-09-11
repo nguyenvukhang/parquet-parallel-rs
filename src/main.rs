c4c1949f074fdd3fae51506ebb6bd4feb6c4ff0f
nguyenvukhang <brew4k@gmail.com>
2024-09-11 23:53:04 +0800

    Actually best

--- src/main.rs
+++ src/main.rs
@@ -69,60 +69,52 @@ fn main() {
 
     let mut total = 0;
     let mut record_batches = vec![];
-    let mut starts = vec![0];
+    let mut chunk_sizes = vec![];
     for rb in reader {
         let rb = rb.unwrap();
-        total += rb.num_rows();
-        starts.push(total);
+        let nrows = rb.num_rows();
+        total += nrows;
+        chunk_sizes.push(nrows);
         record_batches.push(rb);
     }
-    starts.pop();
 
     let mut traces = Vec::with_capacity(total);
     traces.resize(total, Trace::default());
 
-    let mut chunks: Vec<_> = traces.chunks_mut(4).collect();
+    let mut chunks = vec![];
+    let mut y = &mut traces[..];
+    for sz in chunk_sizes.clone() {
+        let (x, z) = y.split_at_mut(sz);
+        chunks.push(x);
+        y = z;
+    }
+
+    let pairs = chunks.into_iter().zip(record_batches).collect::<Vec<_>>();
 
     pool.install(|| {
-        chunks.par_iter_mut().for_each(|v| {
-            v[0] = Trace {
-                timestamp: 123,
-                addr: 0,
-                size: 123,
-                kind: TraceKind::Read,
+        pairs.into_par_iter().for_each(|(trace, mut rb)| {
+            let x = rb.remove_column(5);
+            let sizes = x.as_any().downcast_ref::<UInt32Array>().unwrap();
+            let x = rb.remove_column(4);
+            let addrs = x.as_any().downcast_ref::<UInt64Array>().unwrap();
+            let x = rb.remove_column(3);
+            let kinds = x.as_any().downcast_ref::<StringArray>().unwrap();
+            let x = rb.remove_column(0);
+            let timestamps = x.as_any().downcast_ref::<UInt64Array>().unwrap();
+            let iter = timestamps.iter().zip(kinds).zip(addrs).zip(sizes);
+
+            for (j, (((t, kind), addr), size)) in iter.enumerate() {
+                let t = t.unwrap();
+                let addr = addr.unwrap();
+                let size = size.unwrap();
+                let kind = match kind.unwrap().as_bytes()[0] {
+                    b'R' => TraceKind::Read,
+                    b'W' => TraceKind::Write,
+                    v => panic!("Invalid trace kind: {v}"),
+                };
+                trace[j] = Trace { timestamp: t, kind, addr, size };
             }
         });
-        // traces.par_iter_mut().enumerate().for_each(|(i, v)| {
-        //     let rb = record_batches.get_mut(i);
-        // });
-        // items.into_par_iter().for_each(|(start, mut rb, ptr)| {
-        //     let x = rb.remove_column(5);
-        //     let sizes = x.as_any().downcast_ref::<UInt32Array>().unwrap();
-        //     let x = rb.remove_column(4);
-        //     let addrs = x.as_any().downcast_ref::<UInt64Array>().unwrap();
-        //     let x = rb.remove_column(3);
-        //     let kinds = x.as_any().downcast_ref::<StringArray>().unwrap();
-        //     let x = rb.remove_column(0);
-        //     let timestamps = x.as_any().downcast_ref::<UInt64Array>().unwrap();
-        //     let iter = timestamps.iter().zip(kinds).zip(addrs).zip(sizes);
-        //
-        //     for (j, (((t, kind), addr), size)) in iter.enumerate() {
-        //         let x = traces.as_mut_ptr();
-        //         let x = unsafe { x.add(start + j) };
-        //
-        //         let t = t.unwrap();
-        //         let addr = addr.unwrap();
-        //         let size = size.unwrap();
-        //         let kind = match kind.unwrap().as_bytes()[0] {
-        //             b'R' => TraceKind::Read,
-        //             b'W' => TraceKind::Write,
-        //             v => panic!("Invalid trace kind: {v}"),
-        //         };
-        //         unsafe {
-        //             *x = Trace { timestamp: t, kind, addr, size };
-        //         }
-        //     }
-        // });
     });
 
     println!("Len: {}", traces.len());
