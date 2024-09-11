fdb9b73016b61b2f3f1f18c8b7ca291eec52f38e
nguyenvukhang <brew4k@gmail.com>
2024-09-11 22:34:57 +0800

    Back to rust

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@ use parquet::file::reader::{FileReader, SerializedFileReader};
 use parquet::record::{reader::RowIter, Row, RowAccessor};
 
 use arrow::array::{AsArray, GenericListArray};
-use arrow_array::Int64Array;
+use arrow_array::UInt64Array;
 
 use std::fs::File;
 use std::path::{Path, PathBuf};
 
 fn main() {
     let data_dir = Path::new("/Users/khang/.local/data/msr-cambridge");
-    let f = File::open(data_dir.join("hm_1.parquet")).unwrap();
+    let f = File::open(data_dir.join("hm_1.typed.parquet")).unwrap();
     let builder = ParquetRecordBatchReaderBuilder::try_new(f).unwrap();
     let mut reader = builder.build().unwrap();
 
     let mut record_batch = reader.next().unwrap().unwrap();
     println!("record_batch: {:?}", record_batch.schema());
     let x = record_batch.remove_column(4);
-    let offset_col = x.as_any().downcast_ref::<Int64Array>().unwrap();
+    let offset_col = x.as_any().downcast_ref::<UInt64Array>().unwrap();
     for x in offset_col {
         println!("{x:?}");
     }
